d00a562a35fdc8c4f34923a90b044cf4943c68d0
ashikurriyal <riyal.ashikur@gmail.com>
2025-05-26 19:01:10 +0600

    Solved: Query written for Problem 2 – Count unique species from sightings

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -52,6 +52,14 @@ INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VA
 
 
 -- 3. Queries (Problems 1–9)
+
+
+
 -- Problem 1: Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
 INSERT INTO rangers (name, region)
 VALUES ('Derek Fox', 'Coastal Plains');
+
+
+-- Problem 2: Count unique species ever sighted.
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings;
